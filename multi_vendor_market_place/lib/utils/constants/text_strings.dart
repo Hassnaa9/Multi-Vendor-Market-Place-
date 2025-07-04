b3a6b0ce1e242c9543ff13a941abcec1c39cf42f
Hassnaa9 <hm2922@fayoum.edu.eg>
2025-07-04 15:23:01 +0300

    add onboarding text strings

---
+++ multi_vendor_market_place/lib/utils/constants/text_strings.dart
@@ -0,0 +1,5 @@
+class TextStrings {
+  static const String onboardingTitle1 = 'Seamless Shopping Experience';
+  static const String onboardingTitle2 = 'Wishlist: Where Fashion Dreams Begin';
+  static const String onboardingTitle3 = 'Swift and Reliable Delivery';
+}
