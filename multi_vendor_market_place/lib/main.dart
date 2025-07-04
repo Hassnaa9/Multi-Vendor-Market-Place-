74702ce742e3be53858a6fe312254c6200485956
Hassnaa9 <hm2922@fayoum.edu.eg>
2025-07-04 14:26:09 +0300

    Add empty directories with .gitkeep

--- multi_vendor_market_place/lib/main.dart
+++ multi_vendor_market_place/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:multi_vendor_market_place/app.dart';
-import 'package:multi_vendor_market_place/utils/theme/theme.dart';
 
 void main() {
   runApp(const App());
--- multi_vendor_market_place/test/widget_test.dart
+++ multi_vendor_market_place/test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:multi_vendor_market_place/app.dart';
 
 import 'package:multi_vendor_market_place/main.dart';
 
