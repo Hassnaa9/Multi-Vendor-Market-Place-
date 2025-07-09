bf5c4c1d005cce3c186667828ad3cbd146189a00
Hassnaa Mohamed <112468126+Hassnaa9@users.noreply.github.com>
2025-07-09 15:50:03 +0300

    Delete multi_vendor_market_place directory

--- multi_vendor_market_place/android/app/src/main/kotlin/com/example/multi_vendor_market_place/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.multi_vendor_market_place
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- multi_vendor_market_place/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:multi_vendor_market_place/app.dart';
-
-void main() {
-  runApp(const App());
-}
-
