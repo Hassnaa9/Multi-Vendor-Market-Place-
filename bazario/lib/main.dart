6e4fa7285eaac249090e0642734bfc469ea67135
asemb <asembarak1010@gmail.com>
2025-07-11 14:50:45 +0300

    image

--- bazario/lib/main.dart
+++ bazario/lib/main.dart
@@ -18,16 +18,26 @@
 //     );
 //   }
 // }
-
-import 'package:bazario/features/location/view/location_page_view.dart';
-import 'package:device_preview/device_preview.dart';
+import 'package:bazario/features/splash&welcome/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => const LocationPageView(),
-    ),
-  );
+  runApp(const BazarioApp());
+}
+
+class BazarioApp extends StatelessWidget {
+  const BazarioApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bazario',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
+      ),
+      home: const SplashScreen(),
+    );
+  }
 }
